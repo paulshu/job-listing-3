b86c13c42e15d5014d655e093fbc9d077506d782
paulshu <paulshu18@gmail.com>
2017-04-20 21:53:27 +0800

    implement step 5

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,9 +2,9 @@ module JobsHelper
 
   def render_job_status(job)
     if job.is_hidden
-      "(Hidden)"
+      content_tag(:span, "", :class => "fa fa-lock fa-lg") #fa-lg是设置图标尺寸的参数
     else
-      "(Public)"
+      content_tag(:span, "", :class => "fa fa-globe fa-lg")
     end
   end
 end
